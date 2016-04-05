d312a4668c98e7231ca249566dd73b431b70f92a
vincent.tarreau <vtarreau@student.42.fr>
2016-04-05 19:58:42 +0200

    ex07 day01 ok

---
+++ day01/ex07/main.cpp
@@ -0,0 +1,53 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   main.cpp                                           :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: vtarreau <vtarreau@student.42.fr>          +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2016/04/05 18:58:55 by vtarreau          #+#    #+#             //
+//   Updated: 2016/04/05 19:57:46 by vtarreau         ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
+
+#include <iostream>
+#include <fstream>
+#include <ostream>
+
+int main(int count, char **args)
+{
+	if (count != 4)
+		std::cout << "You need one filename and two arguments" << std::endl;
+	else 
+	{
+		std::string fileName = args[1];
+		std::string s1 = args[2];
+		std::string s2 = args[3];
+
+		if (fileName.length() == 0 || s2.length() == 0 || s2.length() == 0)
+			std::cout << "Your arguments cannot be empty" << std::endl;
+		else 
+		{
+			std::ifstream readFile;
+			std::ofstream writeFile;
+
+			readFile.open(fileName);
+			writeFile.open(fileName + ".replace");
+
+			std::string line;
+			size_t len = s1.length();
+
+			while (std::getline(readFile, line))
+			{
+				size_t pos = line.find(s1);
+				if (pos != std::string::npos)
+					line.replace(pos, len, s2);
+				writeFile << line << std::endl;
+			}
+
+			readFile.close();
+			writeFile.close();	
+		}
+	}
+	return (0);
+}
